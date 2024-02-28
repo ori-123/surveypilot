7a3749a1f10402345189cdc1ef0edbda0ebd510e
oridev-123 <124574469+ori-123@users.noreply.github.com>
2024-02-29 00:42:05 +0100

    map main components to routes, remove App.jsx (and .css), use Login.jsx instead

--- sp-frontend/src/router.jsx
+++ sp-frontend/src/router.jsx
@@ -1,10 +1,25 @@
 import {createBrowserRouter} from "react-router-dom";
-import App from "./App.jsx";
+import Dashboard from "./views/Dashboard.jsx";
+import Surveys from "./views/Surveys.jsx";
+import Login from "./views/Login.jsx";
+import Signup from "./views/Signup.jsx";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App />
+        element: <Dashboard />
+    },
+    {
+        path: '/surveys',
+        element: <Surveys />
+    },
+    {
+        path: '/login',
+        element: <Login />
+    },
+    {
+        path: '/signup',
+        element: <Signup />
     }
 ])
 
