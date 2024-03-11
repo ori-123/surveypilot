d4a8711fb0b8f7cb3ff2875e899af4e815c1131d
oridev-123 <124574469+ori-123@users.noreply.github.com>
2024-03-11 12:53:36 +0100

    implement react context and further frontend functionality for dashboard and surveys components

--- sp-frontend/src/components/DefaultLayout.jsx
+++ sp-frontend/src/components/DefaultLayout.jsx
@@ -1,27 +1,38 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { NavLink, Outlet } from "react-router-dom";
+import {Bars3Icon, BellIcon, UserIcon, XMarkIcon} from '@heroicons/react/24/outline'
+import {Navigate, NavLink, Outlet} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
+/*
 const user = {
     name: 'Tom Cook',
     email: 'tom@example.com',
     imageUrl:
         'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
+}*/
 const navigation = [
     { name: 'Dashboard', to: '/' },
     { name: 'Surveys', to: '/surveys' },
 ]
-const userNavigation = [
-    { name: 'Sign out', href: '#' },
-]
+
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
 export default function DefaultLayout() {
+    const { currentUser, userToken } = useStateContext();
+
+    if (!userToken) {
+        return <Navigate  to={'/login'} />
+    }
+
+    const logout = (ev) => {
+        ev.preventDefault();
+        console.log("Logout");
+    }
+
     return (
         <>
             <div className="min-h-full">
@@ -41,33 +52,22 @@ export default function DefaultLayout() {
                                         <div className="hidden md:block">
                                             <div className="ml-10 flex items-baseline space-x-4">
                                                 {navigation.map((item) => (
-                                                    <a
+                                                    <NavLink
                                                         key={item.name}
-                                                        href={item.href}
-                                                        className={classNames(
-                                                            item.current
-                                                                ? 'bg-gray-900 text-white'
-                                                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                            'rounded-md px-3 py-2 text-sm font-medium'
+                                                        to={item.to}
+                                                        className={({ isActive }) => classNames(
+                                                            isActive ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                            'block rounded-md px-3 py-2 text-base font-medium'
                                                         )}
-                                                        aria-current={item.current ? 'page' : undefined}
                                                     >
                                                         {item.name}
-                                                    </a>
+                                                    </NavLink>
                                                 ))}
                                             </div>
                                         </div>
                                     </div>
                                     <div className="hidden md:block">
                                         <div className="ml-4 flex items-center md:ml-6">
-                                            <button
-                                                type="button"
-                                                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                            >
-                                                <span className="absolute -inset-1.5" />
-                                                <span className="sr-only">View notifications</span>
-                                                <BellIcon className="h-6 w-6" aria-hidden="true" />
-                                            </button>
 
                                             {/* Profile dropdown */}
                                             <Menu as="div" className="relative ml-3">
@@ -75,7 +75,7 @@ export default function DefaultLayout() {
                                                     <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                                         <span className="absolute -inset-1.5" />
                                                         <span className="sr-only">Open user menu</span>
-                                                        <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
+                                                        <UserIcon className={"w-8 h-8 bg-black/25 p-2 rounded-full text-white"} />
                                                     </Menu.Button>
                                                 </div>
                                                 <Transition
@@ -88,21 +88,16 @@ export default function DefaultLayout() {
                                                     leaveTo="transform opacity-0 scale-95"
                                                 >
                                                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                                        {userNavigation.map((item) => (
-                                                            <Menu.Item key={item.name}>
-                                                                {({ active }) => (
+
+                                                            <Menu.Item>
                                                                     <a
-                                                                        href={item.href}
-                                                                        className={classNames(
-                                                                            active ? 'bg-gray-100' : '',
-                                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                                        )}
+                                                                        href='#'
+                                                                        onClick={(ev) => logout(ev)}
+                                                                        className={'block px-4 py-2 text-sm text-gray-700'}
                                                                     >
-                                                                        {item.name}
+                                                                        Sign out
                                                                     </a>
-                                                                )}
                                                             </Menu.Item>
-                                                        ))}
                                                     </Menu.Items>
                                                 </Transition>
                                             </Menu>
@@ -141,32 +136,22 @@ export default function DefaultLayout() {
                                 <div className="border-t border-gray-700 pb-3 pt-4">
                                     <div className="flex items-center px-5">
                                         <div className="flex-shrink-0">
-                                            <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                                            <UserIcon className={"w-8 h-8 bg-black/25 p-2 rounded-full text-white"}/>
                                         </div>
                                         <div className="ml-3">
-                                            <div className="text-base font-medium leading-none text-white">{user.name}</div>
-                                            <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
+                                            <div className="text-base font-medium leading-none text-white">{currentUser.name}</div>
+                                            <div className="text-sm font-medium leading-none text-gray-400">{currentUser.email}</div>
                                         </div>
-                                        <button
-                                            type="button"
-                                            className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                        >
-                                            <span className="absolute -inset-1.5" />
-                                            <span className="sr-only">View notifications</span>
-                                            <BellIcon className="h-6 w-6" aria-hidden="true" />
-                                        </button>
                                     </div>
                                     <div className="mt-3 space-y-1 px-2">
-                                        {userNavigation.map((item) => (
                                             <Disclosure.Button
-                                                key={item.name}
                                                 as="a"
-                                                href={item.href}
+                                                href="#"
+                                                onClick={(ev) => logout(ev)}
                                                 className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
                                             >
-                                                {item.name}
+                                                Sign out
                                             </Disclosure.Button>
-                                        ))}
                                     </div>
                                 </div>
                             </Disclosure.Panel>
