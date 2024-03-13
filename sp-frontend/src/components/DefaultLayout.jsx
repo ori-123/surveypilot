25d0e57c25947983347f9404c52a80e1810a90dc
oridev-123 <124574469+ori-123@users.noreply.github.com>
2024-03-13 23:55:36 +0100

    get user information in layout

--- sp-frontend/src/components/DefaultLayout.jsx
+++ sp-frontend/src/components/DefaultLayout.jsx
@@ -1,41 +1,48 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import {Bars3Icon, BellIcon, UserIcon, XMarkIcon} from '@heroicons/react/24/outline'
-import {Navigate, NavLink, Outlet} from "react-router-dom";
-import {useStateContext} from "../contexts/ContextProvider.jsx";
-import axiosClient from "../axios.js";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import {
+    Bars3Icon,
+    BellIcon,
+    UserIcon,
+    XMarkIcon,
+} from "@heroicons/react/24/outline";
+import { Navigate, NavLink, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
+import axiosClient from "../axios";
+import { useEffect } from "react";
+import Toast from "./Toast";
 
-/*
-const user = {
-    name: 'Tom Cook',
-    email: 'tom@example.com',
-    imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}*/
 const navigation = [
-    { name: 'Dashboard', to: '/' },
-    { name: 'Surveys', to: '/surveys' },
-]
-
+    { name: "Dashboard", to: "/" },
+    { name: "Surveys", to: "/surveys" },
+];
 
 function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
+    return classes.filter(Boolean).join(" ");
 }
 
 export default function DefaultLayout() {
-    const { currentUser, userToken, setCurrentUser, setUserToken } = useStateContext();
+    const { currentUser, userToken, setCurrentUser, setUserToken } =
+        useStateContext();
 
     if (!userToken) {
-        return <Navigate  to={'/login'} />
+        return <Navigate to="login" />;
     }
 
     const logout = (ev) => {
         ev.preventDefault();
-        axiosClient.post('/logout').then(res => {
-                setCurrentUser({});
-                setUserToken(null);
+        axiosClient.post("/logout").then((res) => {
+            setCurrentUser({});
+            setUserToken(null);
+        });
+    };
+
+    useEffect(() => {
+        axiosClient.get('/me')
+            .then(({ data }) => {
+                setCurrentUser(data)
             })
-    }
+    }, [])
 
     return (
         <>
@@ -59,10 +66,14 @@ export default function DefaultLayout() {
                                                     <NavLink
                                                         key={item.name}
                                                         to={item.to}
-                                                        className={({ isActive }) => classNames(
-                                                            isActive ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                            'block rounded-md px-3 py-2 text-base font-medium'
-                                                        )}
+                                                        className={({ isActive }) =>
+                                                            classNames(
+                                                                isActive
+                                                                    ? "bg-gray-900 text-white"
+                                                                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                                                "px-3 py-2 rounded-md text-sm font-medium"
+                                                            )
+                                                        }
                                                     >
                                                         {item.name}
                                                     </NavLink>
@@ -72,14 +83,12 @@ export default function DefaultLayout() {
                                     </div>
                                     <div className="hidden md:block">
                                         <div className="ml-4 flex items-center md:ml-6">
-
                                             {/* Profile dropdown */}
                                             <Menu as="div" className="relative ml-3">
                                                 <div>
-                                                    <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                                                        <span className="absolute -inset-1.5" />
+                                                    <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                                         <span className="sr-only">Open user menu</span>
-                                                        <UserIcon className={"w-8 h-8 bg-black/25 p-2 rounded-full text-white"} />
+                                                        <UserIcon className="w-8 h-8 bg-black/25 p-2 rounded-full text-white" />
                                                     </Menu.Button>
                                                 </div>
                                                 <Transition
@@ -92,16 +101,17 @@ export default function DefaultLayout() {
                                                     leaveTo="transform opacity-0 scale-95"
                                                 >
                                                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-
-                                                            <Menu.Item>
-                                                                    <a
-                                                                        href='#'
-                                                                        onClick={(ev) => logout(ev)}
-                                                                        className={'block px-4 py-2 text-sm text-gray-700'}
-                                                                    >
-                                                                        Sign out
-                                                                    </a>
-                                                            </Menu.Item>
+                                                        <Menu.Item>
+                                                            <a
+                                                                href="#"
+                                                                onClick={(ev) => logout(ev)}
+                                                                className={
+                                                                    "block px-4 py-2 text-sm text-gray-700"
+                                                                }
+                                                            >
+                                                                Sign out
+                                                            </a>
+                                                        </Menu.Item>
                                                     </Menu.Items>
                                                 </Transition>
                                             </Menu>
@@ -109,13 +119,18 @@ export default function DefaultLayout() {
                                     </div>
                                     <div className="-mr-2 flex md:hidden">
                                         {/* Mobile menu button */}
-                                        <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                                            <span className="absolute -inset-0.5" />
+                                        <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                             <span className="sr-only">Open main menu</span>
                                             {open ? (
-                                                <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                                                <XMarkIcon
+                                                    className="block h-6 w-6"
+                                                    aria-hidden="true"
+                                                />
                                             ) : (
-                                                <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                                                <Bars3Icon
+                                                    className="block h-6 w-6"
+                                                    aria-hidden="true"
+                                                />
                                             )}
                                         </Disclosure.Button>
                                     </div>
@@ -123,39 +138,47 @@ export default function DefaultLayout() {
                             </div>
 
                             <Disclosure.Panel className="md:hidden">
-                                <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                                <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
                                     {navigation.map((item) => (
                                         <NavLink
                                             key={item.name}
                                             to={item.to}
-                                            className={({ isActive }) => classNames(
-                                                isActive ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                'block rounded-md px-3 py-2 text-base font-medium'
-                                            )}
+                                            className={({ isActive }) =>
+                                                classNames(
+                                                    isActive
+                                                        ? "bg-gray-900 text-white"
+                                                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                                    "block px-3 py-2 rounded-md text-base font-medium"
+                                                )
+                                            }
                                         >
                                             {item.name}
                                         </NavLink>
                                     ))}
                                 </div>
-                                <div className="border-t border-gray-700 pb-3 pt-4">
+                                <div className="border-t border-gray-700 pt-4 pb-3">
                                     <div className="flex items-center px-5">
                                         <div className="flex-shrink-0">
-                                            <UserIcon className={"w-8 h-8 bg-black/25 p-2 rounded-full text-white"}/>
+                                            <UserIcon className="w-8 h-8 bg-black/25 p-2 rounded-full text-white" />
                                         </div>
                                         <div className="ml-3">
-                                            <div className="text-base font-medium leading-none text-white">{currentUser.name}</div>
-                                            <div className="text-sm font-medium leading-none text-gray-400">{currentUser.email}</div>
+                                            <div className="text-base font-medium leading-none text-white">
+                                                {currentUser.name}
+                                            </div>
+                                            <div className="text-sm font-medium leading-none text-gray-400">
+                                                {currentUser.email}
+                                            </div>
                                         </div>
                                     </div>
                                     <div className="mt-3 space-y-1 px-2">
-                                            <Disclosure.Button
-                                                as="a"
-                                                href="#"
-                                                onClick={(ev) => logout(ev)}
-                                                className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                                            >
-                                                Sign out
-                                            </Disclosure.Button>
+                                        <Disclosure.Button
+                                            as="a"
+                                            href="#"
+                                            onClick={(ev) => logout(ev)}
+                                            className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                                        >
+                                            Sign out
+                                        </Disclosure.Button>
                                     </div>
                                 </div>
                             </Disclosure.Panel>
@@ -165,7 +188,8 @@ export default function DefaultLayout() {
 
                 <Outlet />
 
+                <Toast />
             </div>
         </>
-    )
+    );
 }
