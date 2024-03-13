393e0af38e6001ce1e2514d74d338461cb95b179
oridev-123 <124574469+ori-123@users.noreply.github.com>
2024-03-13 10:18:19 +0100

    create models and migrations with artisan

--- sp-backend/app/Http/Controllers/SurveyController.php
+++ sp-backend/app/Http/Controllers/SurveyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SurveyStoreRequest;
 use App\Http\Requests\SurveyUpdateRequest;
+use App\Models\Survey;
 use Illuminate\Http\Request;
 
 class SurveyController extends Controller
