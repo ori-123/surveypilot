c831e6aced68596af3998924549c2deb2fe52c9f
oridev-123 <124574469+ori-123@users.noreply.github.com>
2024-03-13 12:32:54 +0100

    finish survey creation implementation

--- sp-backend/app/Http/Resources/SurveyResource.php
+++ sp-backend/app/Http/Resources/SurveyResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 
 class SurveyResource extends JsonResource
 {
@@ -14,6 +15,17 @@ class SurveyResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'image_url' => $this->image ? URL::to($this->image) : null,
+            'status' => !!$this->status,
+            'description' => $this->description,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+            'expire_date' => (new \DateTime($this->expire_date))->format('Y-m-d'),
+            'questions' => SurveyQuestionResource::collection($this->questions)
+        ];
     }
 }
--- sp-backend/app/Models/Survey.php
+++ sp-backend/app/Models/Survey.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 /**
  * @method static where(string $string, $id)
@@ -11,4 +13,12 @@ use Illuminate\Database\Eloquent\Model;
 class Survey extends Model
 {
     use HasFactory;
+    use HasSlug;
+
+    protected $fillable = ['title', 'description', 'expire_date', 'image', 'user_id', 'status', 'created_at', 'updated_at'];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()->generateSlugsFrom('title')->saveSlugsTo('slug');
+    }
 }
